de992b9b8fcce11f12d475bf1f497e820e3f2126
ItzFelice <felicelim26@gmail.com>
2022-01-09 04:12:33 +0000

    done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
     import supabase from '$lib/db';
+    import Login from '$lib/login.svelte';
+ import Signup from '$lib/signup.svelte';
 
     async function logout() {
    	 const { error } = await supabase.auth.signOut();
@@ -13,3 +15,6 @@
 <section class="container px-4 py-3 text-center">
     <button class="btn btn-secondary" on:click={logout}>Logout</button>
 </section>	
+
+<Signup/>
+<Login/>
